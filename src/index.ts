2cf205aa8ef161ee88a4ce3306839235a84d8472
khoivudev <vuhuykhoi1997@gmail.com>
2022-06-17 11:45:26 +0900

    [feat] Implement User model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.set({ name: 'newname', age: 9999 });
+user.set({ name: 'Alex', age: 9999 });
+user.set({ age: 9999, name: 'Alex' });
+user.set({});
+console.log(user.get('name'));
+console.log(user.get('age'));
