882422d01fac1f78c959ae8f8ea96771476e509f
khoivudev <vuhuykhoi1997@gmail.com>
2022-06-17 22:30:19 +0900

    [refactor] Seperate Event and Sync class

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ import { User } from './models/User';
 const user = new User({});
 user.set({ name: 'NEW NAME' });
 user.set({ age: 24 });
-user.save();
+
+user.events.on('change', () => {
+  console.log('change!');
+});
+
+user.events.trigger('change');
