c97537ab10f228c05a76b2329383cad428a46e5e
khoivudev <vuhuykhoi1997@gmail.com>
2022-06-17 14:48:10 +0900

    [feat] Implement user model axios

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 import { User } from './models/User';
-import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20 });
-
-user.on('change', () => {
-  console.log('change #1');
-});
-user.on('change', () => {
-  console.log('change #2');
-});
-user.on('save', () => {
-  console.log('save was triggered');
-});
-
-user.trigger('change');
-
-axios.get('http://localhost:3000/users/1');
+const user = new User({});
+user.set({ name: 'NEW NAME' });
+user.set({ age: 24 });
+user.save();
