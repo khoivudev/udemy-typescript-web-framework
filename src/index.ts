4802eacdd151c286e1ceff193859494d3083699c
khoivudev <vuhuykhoi1997@gmail.com>
2022-06-18 20:43:38 +0900

    [feat] Implement Collection Class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { User } from './models/User';
 
-const user = User.buildUser({ id: 1 });
-
-user.on('change', () => {
-  console.log(user);
+const collection = User.buildUserCollection();
+collection.on('change', () => {
+  console.log(collection);
 });
 
-user.fetch();
+collection.fetch();
